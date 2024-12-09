acfb280e0becf3166873ad729c31305664a59f1b
Grazia <g.dipietro4@studenti.unisa.it>
2024-12-09 10:07:00 +0100

    Country/Education
    Codice relativo alle distribuzioni per paese e livello di educazione

--- grafici_Country.R
+++ grafici_Country.R
@@ -1,21 +1,38 @@
 #seguirà l'analisi dei paesi cinvolti nell'indagine co annessi grafici
 
 data<-read.csv("drug_consumption.csv",sep=",",header = T)
-
 #ordino in base all'affluenza maggiore i paesi per poi farne un grafico a torta
-Paesi<-ordered(data$Country,levels=c("-0.09765","0.24923","-0.46841","-0.28519","0.21128","0.96082","-0.57009"))
+#Paesi<-ordered(data$Country,levels=c("-0.09765","0.24923","-0.46841","-0.28519","0.21128","0.96082","-0.57009"))
+#table(Paesi)
+
+#cambio il nome dei dati che riferiscono il tipo di educazione(CITATA SOPRA):
+data$Country <- ifelse(data$Country == "-0.09765", "Australia",
+                         ifelse(data$Country == "0.24923", "Canada",
+                                ifelse(data$Country == "-0.46841", "Nuova Zelanda",
+                                       ifelse(data$Country == "-0.28519", "Altri",
+                                              ifelse(data$Country == "0.21128", "Irlanda",
+                                                     ifelse(data$Country == "0.96082", "UK",
+                                                            ifelse(data$Country == "-0.57009", "USA",
+                                                                                 data$Country))))))) # Mantieni il valore originale per altri casi
+
+Paesi<-ordered(data$Country,levels=c("Australia","Canada","Nuova Zelanda","Altri","Irlanda","UK","USA"))
 table(Paesi)
 View(table(Paesi))
 
 tablePaesi<-table(Paesi)
 
-# grafico a barre
-png("Paesi_bar_chart.png", width = 800, height = 800, res = 150)
 
+
+
+
+
+# grafico a barre
+#png("Paesi_bar_chart.png", width = 800, height = 800, res = 150)
 barplot(tablePaesi,col =c("orange","green","blue","violet","red","yellow","grey"))
 
-# grafico a torta
-png("Paesi_pie_chart.png", width = 800, height = 800, res = 150)
 
+
+# grafico a torta
+#png("Paesi_pie_chart.png", width = 800, height = 800, res = 150)
 pie(tablePaesi, col = c("orange","green","blue","violet","red","yellow","grey"),
     main = "Distribuzione delle fasce d'età")
--- grafici_Education.R
+++ grafici_Education.R
@@ -1,24 +1,26 @@
-#seguirà l'analisi dei paesi cinvolti nell'indagine co annessi grafici
 
-data<-read.csv("drug_consumption.csv",sep=",",header = T)
-
-#provo un altro metodo poichè il calcolo l'ho già fatto in età e paese
-educazione<-c(rep("lasciata a <16",28),rep(" lasciata a 16 ",99),rep("lasciata a 17 ",30),rep("lasciata a 18 ",100),rep(),rep("non certificati/rinuncia",506),rep("diploma",270),rep("università",480),rep("master",283),rep("dottorato",89))
-View(table(educazione))
-
-#***********************************************************************************
-#da aggiustare********************************************************
-titolo<-ordered(educazione,levels=c("lasciata a meno di 16","lasciata a 16","lasciata a 17","lasciata a 18","nessun certificato","diploma","università","master","dottorato"))
-View(table(titolo))
-#***********************************************************************************
-
-# grafico a barre
-png("Scuola_bar_chart.png", width = 800, height = 800, res = 150)
-
-plot(titolo,xlab="Livello di studio posseduto.",col =c("orange","pink","coral","green","blue","violet","red","yellow","grey"))
-
-# grafico a torta
-png("Scuola_pie_chart.png", width = 800, height = 800, res = 150)
-
-pie(titolo, col = c("orange","pink","coral","green","blue","violet","red","yellow","grey"),
-    main = "Distribuzione dell'educazione scolastica")
+  #seguirà l'analisi dei paesi cinvolti nell'indagine co annessi grafici
+  
+  data<-read.csv("drug_consumption.csv",sep=",",header = T)
+  
+  #provo un altro metodo poichè il calcolo l'ho già fatto in età e paese
+  educazione<-c(rep("lasciata_<16",28),rep("lasciata_16",99),rep("lasciata_17",30),rep("lasciata_18 ",100),rep(),rep("senza_certificato",506),rep("diploma",270),rep("università",480),rep("master",283),rep("dottorato",89))
+  View(table(educazione))
+  #***********************************************************************************
+  titolo<-ordered(educazione,levels=c("lasciata_<16","lasciata_16","lasciata_17","lasciata_18 ","senza_certificato","diploma","università","master","dottorato"))
+  View(table(titolo))
+  #***********************************************************************************
+  tableTitolo<-table(titolo)
+  
+  
+  # grafico a barre
+  #png("Scuola_bar_chart.png", width = 800, height = 800, res = 150)
+  
+  plot(tableTitolo,xlab="Livello di studio posseduto.",col =c("orange","pink","coral","green","blue","violet","red","yellow","grey"),ylim =c(0,800))
+  
+  # grafico a torta
+  #png("Scuola_pie_chart.png", width = 800, height = 800, res = 150)
+  
+  pie(tableTitolo, col = c("orange","pink","coral","green","blue","violet","red","yellow","grey"),
+      main = "Distribuzione dell'educazione scolastica")
+  
